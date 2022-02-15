3bc7c3ceca677043eb1a94cdb9847c6d54012048
smic <smic@smic.com>
2022-02-15 23:02:22 +0100

    Improve IGC parsing

--- src/Parser/TrackParser/IGCParser.py
+++ src/Parser/TrackParser/IGCParser.py
@@ -10,8 +10,10 @@ import re
 RE_DATE_LINE = re.compile("HFDTEDATE:([0-9]{2})([0-9]{2})([0-9]{2})\n")
 RE_DATE_LINE2 = re.compile("HFDTE([0-9]{2})([0-9]{2})([0-9]{2})\n")
 RE_PILOT_NAME_LINE = re.compile("HFPLTPILOT:(.*)\n")
+RE_PILOT_NAME_LINE2 = re.compile("HFPLTPILOTINCHARGE:(.*)\n")
+RE_GLIDER_NAME_LINE = re.compile("HFGIDGLIDERID:(.*)\n")
 RE_GPS_REFERENCE_LINE = re.compile("HFDTM100GPSDATUM:(.*)\n")
-RE_B_LINE = re.compile("B([0-9]{2})([0-9]{2})([0-9]{2})(.{2})(.{5})([N|S])(.{3})(.{5})([W|E])[A|V](.{5})(.{5})\n")
+RE_B_LINE = re.compile("B([0-9]{2})([0-9]{2})([0-9]{2})(.{2})(.{5})([N|S])(.{3})(.{5})([W|E])[A|V](-.{4}|.{5})(.{5})")
 
 ### CLASSES
 class IGCParser(TrackParser):
@@ -22,7 +24,8 @@ class IGCParser(TrackParser):
 
     def parse(self):
         date = None
-        pilotName = None
+        pilotName = ""
+        gliderName = ""
         gpsReference = None
         coordinates = []
 
@@ -41,13 +44,25 @@ class IGCParser(TrackParser):
                         raise RuntimeError("Parsing failed: HFDTE is not valid")
                     date = datetime.date(int(match.group(3))+2000, int(match.group(2)), int(match.group(1)))
 
-                elif(line.startswith("HFPLTPILOT")):
+                elif(line.startswith("HFPLTPILOT:")):
                     match = RE_PILOT_NAME_LINE.match(line)
                     if(not match):
                         raise RuntimeError("Parsing failed: HFPLTPILOT is not valid")
                     pilotName = match.group(1)
 
-                elif(line.startswith("HFDTM100GPSDATUM")):
+                elif(line.startswith("HFPLTPILOTINCHARGE:")):
+                    match = RE_PILOT_NAME_LINE2.match(line)
+                    if(not match):
+                        raise RuntimeError("Parsing failed: HFPLTPILOTINCHARGE is not valid")
+                    pilotName = match.group(1)
+
+                elif(line.startswith("HFGIDGLIDERID:")):
+                    match = RE_GLIDER_NAME_LINE.match(line)
+                    if(not match):
+                        raise RuntimeError("Parsing failed: HFGIDGLIDERID is not valid")
+                    gliderName = match.group(1)
+
+                elif(line.startswith("HFDTM100GPSDATUM:")):
                     match = RE_GPS_REFERENCE_LINE.match(line)
                     if(not match):
                         raise RuntimeError("Parsing failed: HFDTM100GPSDATUM is not valid")
@@ -59,7 +74,7 @@ class IGCParser(TrackParser):
 
                 line = inputFile.readline()
 
-        track = Track(pilotName, "", date, gpsReference, coordinates)
+        track = Track(pilotName, gliderName, date, gpsReference, coordinates)
         return track
 
 
